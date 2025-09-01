6f931192b79479656d2004d2d41c90443e3ae874
Diego Vera <diegoveralejandro68@gmail.com>
2025-09-01 10:37:39 -0300

    Modifique el find para aplicarlo en una funcion

--- src/Testing.js
+++ src/Testing.js
@@ -1,8 +1,11 @@
-a = [3,4,12,5,8,12]
+const a = [3,4,12,5,8,12]
 const c = a.find(x => x >= 15)
 
-if (c == undefined){
-  return console.log("No Se Ha Encontrado El Numero Mayor A 15")
-} else{
-  return console.log(c)
+const d = (c)=>{
+  if (c == undefined){
+    return "No Se Ha Encontrado El Numero Mayor A 15"
+  } else{
+    return c
+  }
 }
+console.log(d(c))
