acf2df62dac2f36e69419e87a77ebfdea376983d
Diego Vera <diegoveralejandro68@gmail.com>
2025-09-24 12:27:35 -0300

    Reacomodando

--- Primer-proyecto/src/Almacenamiento.js
+++ Primer-proyecto/src/Almacenamiento.js
@@ -1,3 +1,27 @@
+//Seccion Comentarios Personales
+
+// Es La Forma De Hacer Un Print por Consola
+console.log("HolaMuchachos")
+
+// Es Como Se Define Una Constante
+const i = 1;
+
+// Es Como Se Define Una Variable
+let b = 2;
+
+// Es Como Se Define Una Función
+function suma (a,b){
+    return console.log(a+b)
+};
+
+// Es Como Una Función Esta Contenida y Lo Imprime
+const o = function (a){
+    return a
+};
+console.log(o(2))
+
+// Otra Forma De Implementar Una Función (Arrow Funtion o Función Flecha)
+const mf = (a,b) => a + b ;
 const javaScrip = (n) => {n*3}; // crea una funcion que multiplica por 3 al valor ingresado
 
 function compose(valuePar){ // crea una funcion que retorna otra funcion,
@@ -68,7 +92,6 @@ const da = (c)=>{
   }
 }
 //console.log(d(c))
-
 
 let val ;
 const t = (val = () => {})
