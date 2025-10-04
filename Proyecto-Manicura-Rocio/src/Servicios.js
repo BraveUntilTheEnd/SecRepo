e1c368d188543232bdb500356646dc61ff052e48
Diego Vera <diegoveralejandro68@gmail.com>
2025-10-04 17:33:51 -0300

    Modelando servivios

---
+++ Proyecto-Manicura-Rocio/src/Servicios.js
@@ -0,0 +1,8 @@
+const productos =[
+        {
+            id: 1, nombre: "Libro",
+            id: 2, nombre: "Compu",
+            id: 3, nombre: "Mono"
+        }
+    ]
+module.exports = {productos}
