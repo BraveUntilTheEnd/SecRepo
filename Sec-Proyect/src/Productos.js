f42547e112964739274655e5b077343eee67ae48
Diego Vera <diegoveralejandro68@gmail.com>
2025-10-05 11:48:35 -0300

    Acomodando Proyectos

--- Sec-Proyect/src/Productos.js
+++
@@ -1,8 +0,0 @@
-const productos =[
-        {
-            id: 1, nombre: "Libro",
-            id: 2, nombre: "Compu",
-            id: 3, nombre: "Mono"
-        }
-    ]
-module.exports = {productos}
