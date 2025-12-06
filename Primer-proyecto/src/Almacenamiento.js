f2d2a45d396816bc3be93e208ce29e9f543c6c7f
Diego Vera <diegoveralejandro68@gmail.com>
2025-12-06 20:41:23 -0300

    Eliminando node Modules y agregando funcion cuadrados al almacenamiento

--- Primer-proyecto/src/Almacenamiento.js
+++ Primer-proyecto/src/Almacenamiento.js
@@ -298,5 +298,36 @@ const imprimirSinParametrosReduce = ()=> {
     return acumulador
 }
 
-console.log (`Menor Precio:`)
-console.log(imprimirSinParametrosReduce())
+//console.log (`Menor Precio:`)
+//console.log(imprimirSinParametrosReduce())
+
+/* dibuja cuadrados con un simbolo y tamaño otorgados
+function drawGift(size, symbol) {
+  let inicio = ""
+  let medio = ""
+  let fin = ""
+  let resultado = "" 
+  if (!(size < 2)){
+      for (let i = 0; i <= size; i++ ) {
+        if (i >= 1 && i != size && size != 2){
+          medio = symbol + " ".repeat(size-2) + symbol + "\n"
+        }else {
+          if (i == size){
+          inicio = symbol.repeat(size)+ "\n"
+        }else {
+          fin = symbol.repeat(size)
+        }}
+}
+    }else{
+      return ""
+    }
+    if(size > 2){
+      resultado = inicio + medio.repeat(size - 2) + fin
+    } else{
+      resultado = inicio + medio + fin
+    }
+  return resultado
+}
+console.log(drawGift(2, "%"))
+console.log(drawGift(3, "%"))
+console.log(drawGift(5, "%"))*/
--- Primer-proyecto/src/index.js
+++ Primer-proyecto/src/index.js
@@ -1,16 +1,30 @@
-const giftsToProduce = {
-    toy: "doll",
-    quantity: 2,
+function drawGift(size, symbol) {
+  let inicio = ""
+  let medio = ""
+  let fin = ""
+  let resultado = "" 
+  if (!(size < 2)){
+      for (let i = 0; i <= size; i++ ) {
+        if (i >= 1 && i != size && size != 2){
+          medio = symbol + " ".repeat(size-2) + symbol + "\n"
+        }else {
+          if (i == size){
+          inicio = symbol.repeat(size)+ "\n"
+        }else {
+          fin = symbol.repeat(size)
+        }}
 }
-
-
-function manufactureGifts(giftsToProduce) {
-  const lista = []
-  giftsToProduce.forEach((e)=> {
-     if (e.quantity >0){
-    lista.push(giftsToProduce.toy)
-  }})
-  
-  return lista
+    
+    }else{
+      return ""
+    }
+    if(size > 2){
+      resultado = inicio + medio.repeat(size - 2) + fin
+    } else{
+      resultado = inicio + medio + fin
+    }
+  return resultado
 }
-console.log(manufactureGifts())
+console.log(drawGift(2, "%"))
+console.log(drawGift(3, "%"))
+console.log(drawGift(5, "%"))
