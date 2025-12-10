5d64628dd9d13fb34c5d9af3414c5618ffff04d4
Diego Vera <diegoveralejandro68@gmail.com>
2025-12-10 14:49:36 -0300

    Agrege Dia 4 AdventJS al almacenamiento

--- Primer-proyecto/src/Almacenamiento.js
+++ Primer-proyecto/src/Almacenamiento.js
@@ -330,4 +330,35 @@ function drawGift(size, symbol) {
 }
 console.log(drawGift(2, "%"))
 console.log(drawGift(3, "%"))
-console.log(drawGift(5, "%"))*/
+console.log(drawGift(5, "%"))
+dia 4 adventjs
+function decodeSantaPin(code) {
+  let codigoFinal = "";
+  const primerFiltro = code.split("]");
+  const primerVerif = primerFiltro.filter(e => e != "");
+  for (let i = 0; i < primerVerif.length; i++) {
+    let segFiltro = primerVerif[i].split("[");
+    let digito = segFiltro[1]; 
+    if (digito == "<") {
+       if (codigoFinal.length > 0) {
+          codigoFinal = codigoFinal + codigoFinal[codigoFinal.length - 1];
+       }
+    } 
+    else {
+       let numBase = parseInt(digito[0]); 
+       for (let j = 1; j < digito.length; j++) {
+          if (digito[j] == "+") numBase++;
+          if (digito[j] == "-") numBase--;
+       }
+       let digitoResuelto = ((numBase % 10) + 10) % 10;
+       codigoFinal = codigoFinal + digitoResuelto;
+    }
+  }
+  if (codigoFinal.length < 4) {
+    return null;
+  }
+  return codigoFinal;
+}
+const codeo = "[1--][1-+][8++][7++]"
+console.log(decodeSantaPin(codeo))*/
+
--- Primer-proyecto/src/index.js
+++ Primer-proyecto/src/index.js
@@ -1,30 +1,32 @@
-function drawGift(size, symbol) {
-  let inicio = ""
-  let medio = ""
-  let fin = ""
-  let resultado = "" 
-  if (!(size < 2)){
-      for (let i = 0; i <= size; i++ ) {
-        if (i >= 1 && i != size && size != 2){
-          medio = symbol + " ".repeat(size-2) + symbol + "\n"
-        }else {
-          if (i == size){
-          inicio = symbol.repeat(size)+ "\n"
-        }else {
-          fin = symbol.repeat(size)
-        }}
-}
-    
-    }else{
-      return ""
+function decodeSantaPin(code) {
+  let codigoFinal = "";
+  const primerFiltro = code.split("]");
+  const primerVerif = primerFiltro.filter(e => e != "");
+  for (let i = 0; i < primerVerif.length; i++) {
+    let segFiltro = primerVerif[i].split("[");
+    let digito = segFiltro[1]; 
+    if (digito == "<") {
+       if (codigoFinal.length > 0) {
+          codigoFinal = codigoFinal + codigoFinal[codigoFinal.length - 1];
+       }
+    } 
+    else {
+       let numBase = parseInt(digito[0]); 
+       for (let j = 1; j < digito.length; j++) {
+          if (digito[j] == "+") numBase++;
+          if (digito[j] == "-") numBase--;
+       }
+       let digitoResuelto = ((numBase % 10) + 10) % 10;
+       codigoFinal = codigoFinal + digitoResuelto;
     }
-    if(size > 2){
-      resultado = inicio + medio.repeat(size - 2) + fin
-    } else{
-      resultado = inicio + medio + fin
-    }
-  return resultado
+  }
+  if (codigoFinal.length < 4) {
+    return null;
+  }
+  return codigoFinal;
 }
-console.log(drawGift(2, "%"))
-console.log(drawGift(3, "%"))
-console.log(drawGift(5, "%"))
+const codeo = "[1--][1-+][8++][7++]"
+console.log(decodeSantaPin(codeo))
+
+
+//Puedo usar un replace para modificar los "-" y "+"
